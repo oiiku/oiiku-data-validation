79d3df2eb13aeab06c7d4d4fb2efc0b10a8a882c
August Lilleaas <august@augustl.com>
2012-11-11 02:19:19 +0100

    Adding url to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject oiiku-data-validation "0.1.0"
   :description "Data validation library"
+  :url "https://github.com/oiiku/oiiku-data-validation"
   :license {:name "New BSD license"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
