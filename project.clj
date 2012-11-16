19b14dd9e8c473eb7fe935cf8ec1870327f83e19
August Lilleaas <august@augustl.com>
2012-11-16 20:35:32 +0100

    Bumping to 0.1.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oiiku-data-validation "0.1.0"
+(defproject oiiku-data-validation "0.1.1"
   :description "Data validation library"
   :url "https://github.com/oiiku/oiiku-data-validation"
   :license {:name "New BSD license"}
