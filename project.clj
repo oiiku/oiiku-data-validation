104aaf49e26868f43caac8ef73827783f55f100e
Anders Furseth <anders.furseth@gmail.com>
2017-01-04 10:30:01 +0100

    Add Oiiku repository for deployment

--- project.clj
+++ project.clj
@@ -2,4 +2,10 @@
   :description "Data validation library"
   :url "https://github.com/oiiku/oiiku-data-validation"
   :license {:name "New BSD license"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :repositories
+  {"oiiku-releases" "http://148.251.86.208:8081/nexus/content/repositories/releases"
+   "oiiku-snapshots" "http://148.251.86.208:8081/nexus/content/repositories/snapshots"}
+  :deploy-repositories
+  {"releases" {:url "http://148.251.86.208:8081/nexus/content/repositories/releases"}
+   "snapshots" {:url "http://148.251.86.208:8081/nexus/content/repositories/snapshots"}})
