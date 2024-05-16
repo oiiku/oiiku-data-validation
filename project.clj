dc2875b51a06ecad9ec9c9a9af8063f7338a34bf
Martin Solli <msolli@gmail.com>
2024-05-16 14:30:35 +0200

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oiiku-data-validation "0.2.0-SNAPSHOT"
+(defproject oiiku-data-validation "0.2.0"
   :description "Data validation library"
   :url "https://github.com/oiiku/oiiku-data-validation"
   :license {:name "New BSD license"}
